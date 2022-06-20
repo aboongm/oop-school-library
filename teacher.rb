9653bf8d7cc289373ce15384563ca580eb1c7cbc
aboongm <aboongm@yahoo.com>
2022-06-20 08:35:55 +0530

    Add teacher class

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require "./person.rb"
+
+class Teacher < Person
+    def initialize(age, name="unknown", parent_permission=true, specialization)
+        super(age, name, parent_permission)
+        @specialization = specialization
+    end
+
+    def can_use_services
+        true
+    end
+end
