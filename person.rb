bfd852cf651cee8af66a2a0851805b491d8596ac
aboongm <aboongm@yahoo.com>
2022-06-20 08:53:39 +0530

    Correct linting errors

--- person.rb
+++ person.rb
@@ -1,24 +1,25 @@
 class Person
-    attr_accessor :name, :age
-    attr_reader :id
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def initialize(age, name = "Unknown", parent_permission = true)
-        @id = Random.rand(1..100)
-        @age = age
-        @name = name
-        @parent_permission = parent_permission
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..100)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
 
-    def can_use_services
-        if is_of_age? || parent_permission
-            true
-        else
-            false
-        end
+  def can_use_services
+    if of_age? || parent_permission
+      true
+    else
+      false
     end
+  end
 
-    private
-    def is_of_age?
-        @age > 18
-    end
+  private
+
+  def of_age?
+    @age > 18
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
-require "./person.rb"
+require './person'
 
 class Teacher < Person
-    def initialize(age, name="unknown", parent_permission=true, specialization)
-        super(age, name, parent_permission)
-        @specialization = specialization
-    end
+  def initialize(age, specialization, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_services
-        true
-    end
+  def can_use_services
+    true
+  end
 end
