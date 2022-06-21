c7767b39bcc38bbc7460ecc4e183553679e7d437
aboongm <aboongm@yahoo.com>
2022-06-21 14:08:42 +0530

    many-to-many relationship between Person and Book using the intermediate class Rental

--- rental.rb
+++ rental.rb
@@ -1,7 +1,12 @@
 class Rental
   attr_accessor :date
+  attr_reader :book, :person
 
-  def initialize(date)
+  def initialize(date, person, book)
     @date = date
+    @person = person
+    person.rentals << self
+    @book = book
+    book.rentals << self
   end
 end
