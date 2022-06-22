87d9a359e6935c63249e766d8525b8b4f256b6d9
aboongm <aboongm@yahoo.com>
2022-06-22 14:17:19 +0530

    Create app.rb

---
+++ app.rb
@@ -0,0 +1,10 @@
+require './book'
+
+class App
+  # list all books
+  # list all people
+  # create a person (teacher or student)
+  # create a book
+  # create a rental
+  # list all rentals for a given person id  
+end
