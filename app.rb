3541dce9620df4707bf2d0b639aafa6885d0af63
aboongm <aboongm@yahoo.com>
2022-06-23 13:48:54 +0530

    Move console interface functionality to main method

--- app.rb
+++ app.rb
@@ -149,22 +149,4 @@ class App
   end
 
   # rubocop:enable Metrics/CyclomaticComplexity
-  def ui_input
-    puts 'Please choose an option by entering a number:'
-    puts '1. list all books'
-    puts '2. list all people'
-    puts '3. create a person (teacher or student)'
-    puts '4. create a book'
-    puts '5. create a rental'
-    puts '6. list all rentals for a given person id'
-    puts '7. Exit'
-
-    option = gets.chomp.to_i
-    handle_input(option)
-  end
-
-  def run
-    puts "Welcome to School Library App\n\n"
-    ui_input
-  end
 end
--- main.rb
+++ main.rb
@@ -2,7 +2,18 @@ require './app'
 
 def main()
   app = App.new
-  app.run
+  puts "Welcome to School Library App\n\n"
+  puts 'Please choose an option by entering a number:'
+  puts '1. list all books'
+  puts '2. list all people'
+  puts '3. create a person (teacher or student)'
+  puts '4. create a book'
+  puts '5. create a rental'
+  puts '6. list all rentals for a given person id'
+  puts '7. Exit'
+
+  option = gets.chomp.to_i
+  app.handle_input(option)
 end
 
 main
