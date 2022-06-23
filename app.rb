51410a268624274f379017459f0ead5b13cb771c
aboongm <aboongm@yahoo.com>
2022-06-23 17:32:23 +0530

    Update main.rb and app.rb

--- app.rb
+++ app.rb
@@ -123,30 +123,4 @@ class App
     end
     puts "\n"
   end
-
-  # rubocop:disable Metrics/CyclomaticComplexity
-  def handle_input(option)
-    case option
-    when 1
-      list_books
-    when 2
-      list_persons
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental
-    when 6
-      list_rentals
-    when 7
-      puts 'Thank you for using School Library App'
-      exit
-    else
-      puts 'Please select an option'
-    end
-    ui_input
-  end
-
-  # rubocop:enable Metrics/CyclomaticComplexity
 end
