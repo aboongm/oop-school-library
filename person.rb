7c0e839c32ec6c87d3464e24458a1733bdbc0a9e
aboongm <aboongm@yahoo.com>
2022-06-23 08:13:15 +0530

    Correction for linter

--- person.rb
+++ person.rb
@@ -10,7 +10,7 @@ class Person < Nameable
     @id = Random.rand(1..1000)
     @age = age
     @name = name
-    @parent_permission = parent_permission    
+    @parent_permission = parent_permission
   end
 
   def can_use_services
