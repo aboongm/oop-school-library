108a89f069d459dd0cef93f6698e906edc10f5e9
aboongm <aboongm@yahoo.com>
2022-06-28 14:06:39 +0530

    Create Basic structure of  dave_data.rb, load_data.rb

---
+++ modules/load_data.rb
@@ -0,0 +1,11 @@
+require './student'
+require './teacher'
+require './rental'
+
+module LoadData
+  def load_books; end
+
+  def load_persons; end
+
+  def load_rentals(persons, books); end
+end
---
+++ modules/save_data.rb
@@ -0,0 +1,17 @@
+require './student'
+require './teacher'
+require './rental'
+
+module SaveData
+  def save_data(books, persons, rentals)
+    # save_books(books)
+    # save_persons(persons)
+    # save_rentals(rentals)
+  end
+
+  def save_books(books); end
+
+  def save_persons(persons); end
+
+  def save_rentals(rentals); end
+end
