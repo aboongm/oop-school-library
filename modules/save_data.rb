c050edbf496c678f01c9484405ac194aca98a909
aboongm <aboongm@yahoo.com>
2022-06-28 14:15:50 +0530

    Update save_books in SaveData modules

--- modules/save_data.rb
+++ modules/save_data.rb
@@ -10,7 +10,15 @@ module SaveData
   end
 
   def save_books(books)
-    
+    saved_books = []
+    books.each do |book|
+      saved_books << {
+        title: book.title,
+        author: book.author
+      }
+    end
+    ruby = JSON.generate(saved_books)
+    File.write('./data/books.json', ruby)
   end
 
   def save_persons(persons)
