35beaffedd22422c631b657276f9243504acd35b
aboongm <aboongm@yahoo.com>
2022-06-28 14:14:42 +0530

    Update the save_data method in SaveData module

--- modules/save_data.rb
+++ modules/save_data.rb
@@ -4,14 +4,20 @@ require './rental'
 
 module SaveData
   def save_data(books, persons, rentals)
-    # save_books(books)
-    # save_persons(persons)
-    # save_rentals(rentals)
+    save_books(books)
+    save_persons(persons)
+    save_rentals(rentals)
   end
 
-  def save_books(books); end
+  def save_books(books)
+    
+  end
 
-  def save_persons(persons); end
+  def save_persons(persons)
+    
+  end
 
-  def save_rentals(rentals); end
+  def save_rentals(rentals)
+    
+  end
 end
