66f47944ea4ef8a7d925d321568a0b51e1960ada
aboongm <aboongm@yahoo.com>
2022-06-28 14:16:16 +0530

    Update save_persons in SaveData modules

--- modules/save_data.rb
+++ modules/save_data.rb
@@ -22,7 +22,24 @@ module SaveData
   end
 
   def save_persons(persons)
-    
+    saved_persons = []
+    puts "persons: #{persons}"
+    persons.each do |person|
+      saved_persons << if person.instance_of? Student
+                         {
+                           json_class: 'Student',
+                           name: person.name,
+                           age: person.age
+                           # parent_permission: person.parent_permission
+                         }
+                       elsif person.instance_of? Teacher
+                         {
+                           json_class: 'Teacher', name: person.name, age: person.age
+                         }
+                       end
+    end
+    ruby = JSON.generate(saved_persons)
+    File.write('./data/persons.json', ruby)
   end
 
   def save_rentals(rentals)
