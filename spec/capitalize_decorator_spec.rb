3b46b0dc84ada358270a4090f0fb54445c3407ea
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-06-29 10:53:46 +0100

    Add test for capitalize_decorator class

---
+++ spec/capitalize_decorator_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../capitalize_decorator'
+require_relative '../person'
+
+describe CapitalizeDecorator do
+  context 'When testing decorator classes' do
+    it 'Should return a string capitalized' do
+      person = Person.new(22, 'maximilianus')
+      capitalized_person = CapitalizeDecorator.new(person)
+      expect(capitalized_person.correct_name).to eq 'Maximilianus'
+    end
+
+  end
+end
